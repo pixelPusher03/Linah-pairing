33555b913d3a196cc2a958c90c153777e2efe254
ᴘɪxᴇʟ Pᴜsʜᴇʀ03 <153280672+pixelPusher03@users.noreply.github.com>
2024-12-22 06:39:07 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,5 +1,5 @@
 const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+pastebin = new PastebinAPI('5f4ilKJVJG-0xbJTXesajw64LgSAAo-L')
 const {makeid} = require('./id');
 const express = require('express');
 const fs = require('fs');
