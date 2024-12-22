427b940ba3d9a6455fec24dac1ae9aa8d135160d
ᴘɪxᴇʟ Pᴜsʜᴇʀ03 <153280672+pixelPusher03@users.noreply.github.com>
2024-12-22 11:54:53 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,5 +1,5 @@
 const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('5f4ilKJVJG-0xbJTXesajw64LgSAAo-L')
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
 const {makeid} = require('./Linah');
 const express = require('express');
 const fs = require('fs');
