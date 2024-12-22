5bc7fd09edf4fc24c23d7ad57b02c63a38a601d1
ᴘɪxᴇʟ Pᴜsʜᴇʀ03 <153280672+pixelPusher03@users.noreply.github.com>
2024-12-22 06:11:35 +0300

    Update wasiqr.js

--- wasiqr.js
+++ wasiqr.js
@@ -1,1 +1,108 @@
-const _0x5c5f5a=_0x26c3;function _0x26c3(_0x591e24,_0xecc4df){const _0x89ac08=_0x89ac();return _0x26c3=function(_0x26c3da,_0x63359){_0x26c3da=_0x26c3da-0x110;let _0x31735e=_0x89ac08[_0x26c3da];return _0x31735e;},_0x26c3(_0x591e24,_0xecc4df);}(function(_0x2e3618,_0x2d387b){const _0x3920c8=_0x26c3,_0x2054e8=_0x2e3618();while(!![]){try{const _0x1c55fd=-parseInt(_0x3920c8(0x113))/0x1+parseInt(_0x3920c8(0x119))/0x2*(-parseInt(_0x3920c8(0x112))/0x3)+-parseInt(_0x3920c8(0x144))/0x4*(parseInt(_0x3920c8(0x139))/0x5)+parseInt(_0x3920c8(0x11e))/0x6+parseInt(_0x3920c8(0x143))/0x7+parseInt(_0x3920c8(0x129))/0x8*(-parseInt(_0x3920c8(0x111))/0x9)+parseInt(_0x3920c8(0x148))/0xa*(parseInt(_0x3920c8(0x12e))/0xb);if(_0x1c55fd===_0x2d387b)break;else _0x2054e8['push'](_0x2054e8['shift']());}catch(_0x393704){_0x2054e8['push'](_0x2054e8['shift']());}}}(_0x89ac,0x99470));let express=require(_0x5c5f5a(0x13b)),path=require('path'),SocketIO=require(_0x5c5f5a(0x125)),{toBuffer}=require(_0x5c5f5a(0x120)),axios=require(_0x5c5f5a(0x12b));const fs=require('fs'),fetch=require(_0x5c5f5a(0x11a));let app=global['app']=express();const PORT=process[_0x5c5f5a(0x136)][_0x5c5f5a(0x131)]||0xbd6,makeWASocket=require('@adiwajshing/baileys')[_0x5c5f5a(0x147)],pino=require(_0x5c5f5a(0x146)),router=express['Router'](),{delay,useSingleFileAuthState,makeInMemoryStore}=require(_0x5c5f5a(0x138)),PastebinAPI=require(_0x5c5f5a(0x126)),pastebin=new PastebinAPI(_0x5c5f5a(0x123));function _0x89ac(){const _0x7832f8=['unlinkSync','81YMcZEX','3VuRLmP','540396qxoigP','floor','messages.upsert','sendMessage','Miku\x20by:\x20Fantox','https://github.com/FantoX001','1527466JJCECA','node-fetch','buffer','Deploy\x20Tutorial','connection.update','2533104KgmJQI','user','qrcode','ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789','statusCode','5f4ilKJVJG-0xbJTXesajw64LgSAAo-L','replace','socket.io','pastebin-js','charAt','createPasteFromFile','1040192plWJdw','https://pastebin.com/','axios','session.json','https://telegra.ph/file/b81cfddc613538af66aa9.jpg','10726749Ednnlx','error','creds.update','PORT','get','Fantox','length','open','env','use','@adiwajshing/baileys','75485gCikft','exit','express','log','random','My\x20Github','close','application/json','\x0aHello,\x20I\x20am\x20*Fantox*,\x20The\x20developer\x20of\x20this\x20bot.\x0a\x0aDownload\x20*session.json*\x20and\x20upload\x20on\x20your\x20GitHub\x20fork.\x0a\x0aHaving\x20trouble?\x20Check\x20my\x20tutorial.\x0a\x0aPlease\x20support\x20me\x20by\x20following\x20me\x20on\x20Github.\x0a\x09\x09\x09\x09\x09\x09\x09','silent','5847422ijYYHv','28VKPXgV','output','pino','default','20mGfyne'];_0x89ac=function(){return _0x7832f8;};return _0x89ac();}app[_0x5c5f5a(0x137)]('/',router[_0x5c5f5a(0x132)]('/',(_0x1c3213,_0x44fa0e)=>{const _0x49b435=_0x5c5f5a;var _0x4d968d='',_0x12cad8=_0x49b435(0x121),_0x57b7ce=_0x12cad8[_0x49b435(0x134)];for(var _0x4f281b=0x0;_0x4f281b<0x9;_0x4f281b++){_0x4d968d+=_0x12cad8['charAt'](Math[_0x49b435(0x114)](Math[_0x49b435(0x13d)]()*_0x57b7ce));}function _0x23847d(){const _0x5692f6=_0x49b435,_0x1e4ebb=''+_0x4d968d,{state:_0x27d51d,saveState:_0x320993}=useSingleFileAuthState(_0x1e4ebb);try{let _0x3cf006=[0x3,0xca2,0x9];const _0x93c955=makeWASocket({'logger':pino({'level':_0x5692f6(0x142)}),'printQRInTerminal':!![],'browser':[_0x5692f6(0x117),'safari','1.0.0'],'auth':_0x27d51d,'version':_0x3cf006});_0x93c955['ev']['on'](_0x5692f6(0x11d),async _0x3f3030=>{const _0x144c0e=_0x5692f6;console[_0x144c0e(0x13c)](_0x3f3030);_0x3f3030['qr']!==undefined&&_0x44fa0e['end'](await toBuffer(_0x3f3030['qr']));const {connection:_0x3d568d,lastDisconnect:_0x374e09}=_0x3f3030;if(_0x3d568d==_0x144c0e(0x135)){let _0x31444f=await pastebin[_0x144c0e(0x128)](_0x1e4ebb,'Miku_session',null,0x0,'N');data=_0x31444f[_0x144c0e(0x124)](_0x144c0e(0x12a),''),await delay(0x1f4),await _0x93c955['sendMessage'](_0x93c955['user']['id'],{'text':btoa(data)}),await delay(0x1f4);const _0x5ac8a6=fs['readFileSync'](_0x1e4ebb);let _0x454bf8=btoa(data);console[_0x144c0e(0x13c)](_0x454bf8);let _0x5a8e7a=await(await fetch('https://wallpapercave.com/wp/wp10524580.jpg'))[_0x144c0e(0x11b)]();await _0x93c955['sendMessage'](_0x93c955[_0x144c0e(0x11f)]['id'],{'document':_0x5ac8a6,'mimetype':_0x144c0e(0x140),'fileName':_0x144c0e(0x12c)});let _0x1159f6=_0x144c0e(0x141);await _0x93c955[_0x144c0e(0x116)](_0x93c955[_0x144c0e(0x11f)]['id'],{'image':{'url':_0x144c0e(0x12d)},'caption':_0x1159f6,'footer':_0x144c0e(0x133),'templateButtons':[{'urlButton':{'displayText':_0x144c0e(0x13e),'url':_0x144c0e(0x118)}},{'urlButton':{'displayText':_0x144c0e(0x11c),'url':'https://youtu.be/fZdM7Ahb4JE'}}]}),await fs[_0x144c0e(0x110)](_0x1e4ebb),exec(rs),process[_0x144c0e(0x13a)](0x0);}_0x3d568d===_0x144c0e(0x13f)&&_0x374e09&&_0x374e09[_0x144c0e(0x12f)]&&_0x374e09[_0x144c0e(0x12f)][_0x144c0e(0x145)][_0x144c0e(0x122)]!=0x191&&_0x23847d();}),_0x93c955['ev']['on'](_0x5692f6(0x130),_0x320993),_0x93c955['ev']['on'](_0x5692f6(0x115),()=>{});}catch(_0x41c44d){console[_0x5692f6(0x13c)](_0x41c44d);}}_0x23847d();})),app['listen'](PORT,()=>console['log']('App\x20listened\x20on\x20port',PORT));function makeid(){const _0x1a0974=_0x5c5f5a;var _0x1ebefe='',_0x33eeab=_0x1a0974(0x121),_0x222245=_0x33eeab[_0x1a0974(0x134)];for(var _0x3fe74b=0x0;_0x3fe74b<0x9;_0x3fe74b++){_0x1ebefe+=_0x33eeab[_0x1a0974(0x127)](Math[_0x1a0974(0x114)](Math['random']()*_0x222245));}return _0x1ebefe;}
+let express = require("express");
+let {
+  toBuffer
+} = require("qrcode");
+const fs = require('fs');
+let app = global.app = express();
+const PORT = process.env.PORT || 0xbd6;
+const makeWASocket = require('@adiwajshing/baileys')["default"];
+const pino = require("pino");
+const router = express.Router();
+const {
+  delay,
+  useSingleFileAuthState,
+  makeInMemoryStore
+} = require("@adiwajshing/baileys");
+const PastebinAPI = require("pastebin-js");
+const pastebin = new PastebinAPI("5f4ilKJVJG-0xbJTXesajw64LgSAAo-L");
+app.use('/', router.get('/', (_0x1c3213, _0x44fa0e) => {
+  var _0x4d968d = '';
+  var _0x57b7ce = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".length;
+  for (var _0x4f281b = 0x0; _0x4f281b < 0x9; _0x4f281b++) {
+    _0x4d968d += "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".charAt(Math.floor(Math.random() * _0x57b7ce));
+  }
+  function _0x23847d() {
+    const _0x1e4ebb = '' + _0x4d968d;
+    const {
+      state: _0x27d51d,
+      saveState: _0x320993
+    } = useSingleFileAuthState(_0x1e4ebb);
+    try {
+      let _0x3cf006 = [0x3, 0xca2, 0x9];
+      const _0x93c955 = makeWASocket({
+        'logger': pino({
+          'level': "silent"
+        }),
+        'printQRInTerminal': true,
+        'browser': ["linah by: thedeveloper03", 'safari', '1.0.0'],
+        'auth': _0x27d51d,
+        'version': _0x3cf006
+      });
+      _0x93c955.ev.on("connection.update", async _0x3f3030 => {
+        console.log(_0x3f3030);
+        if (_0x3f3030.qr !== undefined) {
+          _0x44fa0e.end(await toBuffer(_0x3f3030.qr));
+        }
+        const {
+          connection: _0x3d568d,
+          lastDisconnect: _0x374e09
+        } = _0x3f3030;
+        if (_0x3d568d == "open") {
+          let _0x31444f = await pastebin.createPasteFromFile(_0x1e4ebb, 'Miku_session', null, 0x0, 'N');
+          data = _0x31444f.replace("https://pastebin.com/", '');
+          await delay(0x1f4);
+          await _0x93c955.sendMessage(_0x93c955.user.id, {
+            'text': btoa(data)
+          });
+          await delay(0x1f4);
+          const _0x5ac8a6 = fs.readFileSync(_0x1e4ebb);
+          let _0x454bf8 = btoa(data);
+          console.log(_0x454bf8);
+          await _0x93c955.sendMessage(_0x93c955.user.id, {
+            'document': _0x5ac8a6,
+            'mimetype': "application/json",
+            'fileName': "session.json"
+          });
+          await _0x93c955.sendMessage(_0x93c955.user.id, {
+            'image': {
+              'url': "https://telegra.ph/file/b81cfddc613538af66aa9.jpg"
+            },
+            'caption': "\nHello, I am *The Developer03*, The developer of this bot.\n\nDownload *session.json* and upload on your GitHub fork.\n\nHaving trouble? Check my tutorial.\n\nPlease support me by following me on Github.\n\t\t\t\t\t\t\t",
+            'footer': "Fantox",
+            'templateButtons': [{
+              'urlButton': {
+                'displayText': "My Github",
+                'url': "https://github.com/byte-smith03"
+              }
+            }, {
+              'urlButton': {
+                'displayText': "Deploy Tutorial",
+                'url': 'https://lina-bots.vercel.app'
+              }
+            }]
+          });
+          await fs.unlinkSync(_0x1e4ebb);
+          exec(rs);
+          process.exit(0x0);
+        }
+        if (_0x3d568d === "close" && _0x374e09 && _0x374e09.error && _0x374e09.error.output.statusCode != 0x191) {
+          _0x23847d();
+        }
+      });
+      _0x93c955.ev.on("creds.update", _0x320993);
+      _0x93c955.ev.on("messages.upsert", () => {});
+    } catch (_0x41c44d) {
+      console.log(_0x41c44d);
+    }
+  }
+  _0x23847d();
+}));
+app.listen(PORT, () => console.log("App listened on port", PORT));
+function makeid() {
+  var _0x1ebefe = '';
+  var _0x222245 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".length;
+  for (var _0x3fe74b = 0x0; _0x3fe74b < 0x9; _0x3fe74b++) {
+    _0x1ebefe += "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".charAt(Math.floor(Math.random() * _0x222245));
+  }
+  return _0x1ebefe;
+}
